85f2ad7f080cc3dad244a7f7e861d85f318ebde0
nerggg <13522147@std.stei.itb.ac.id>
2024-02-26 15:24:39 +0700

    add: bot dasar dan modif bot autowin

--- autowin.py
+++ autowin.py
@@ -27,22 +27,46 @@ class AutowinLogic(object):
             ## perhitungkan diamond kotak merah
             ### kalo diamond merah dan biru kejauhan, incar diamond kotak
 
-            # for i in range(len(board.game_objects)):
-            #     print("ini anggota board nya ", board.game_objects[i].type)
+            for i in range (len(board.game_objects)):
+                print(board.game_objects[i].type)
 
             min = int(0)
-            for i in range (len(board.diamonds)):
-                xMinDist = (abs(board_bot.position.x - board.diamonds[min].position.x)) 
-                yMinDist = (abs(board_bot.position.y - board.diamonds[min].position.y)) 
-                xNewDist = (abs(board_bot.position.x - board.diamonds[i].position.x)) 
-                yNewDist = (abs(board_bot.position.y - board.diamonds[i].position.y)) 
-                minDist = math.sqrt(xMinDist * xMinDist + yMinDist * yMinDist)
-                newDist = math.sqrt(xNewDist * xNewDist + yNewDist + yNewDist)
+            homeDistanceToNearestDiamond = float(0)
+            diamondButton = int()
+            home = int()
+            for i in range(len(board.game_objects)):
+                if (board.game_objects[i].type == "BaseGameObject"):
+                    home = i
+                    continue
+                elif (board.game_objects[i].type == "DiamondButtonGameObject"):
+                    diamondButton = i
+                    diamondButtonDistanceToHome = math.sqrt(math.pow(board.game_objects[home].position.x - board.game_objects[i].position.x, 2) + math.pow(board.game_objects[home].position.y - board.game_objects[i].position.y, 2))
+                    continue
+                elif (board.game_objects[i].type == "BotGameObject" and board_bot.position.x == board.game_objects[i].position.x and board_bot.position.y == board.game_objects[i].position.y):
+                    continue
+                elif (board.game_objects[i].type == "DiamondGameObject"):
+                    if (board_bot.properties.diamonds + board.game_objects[i].properties.points > 5):
+                        continue
+                    xMinDist = (abs(board_bot.position.x - board.game_objects[min].position.x)) 
+                    yMinDist = (abs(board_bot.position.y - board.game_objects[min].position.y)) 
+                    xNewDist = (abs(board_bot.position.x - board.game_objects[i].position.x)) 
+                    yNewDist = (abs(board_bot.position.y - board.game_objects[i].position.y)) 
+                    minDist = math.sqrt(xMinDist * xMinDist + yMinDist * yMinDist)
+                    newDist = math.sqrt(xNewDist * xNewDist + yNewDist + yNewDist)
 
-                if (newDist < minDist):
-                    min = i
+                    if (newDist < minDist):
+                        min = i
+                        homeDistanceToNearestDiamond = math.sqrt(math.pow(board.game_objects[home].position.x - board.game_objects[min].position.x, 2) + math.pow(board.game_objects[home].position.y - board.game_objects[min].position.y, 2))
+                # elif (board.game_objects[i].type == "TeleportGameObject"):
 
-            self.goal_position = board.diamonds[min].position
+
+
+
+            # if (homeDistanceToNearestDiamond > math.sqrt(72) or homeDistanceToNearestDiamond > diamondButtonDistanceToHome):
+            if (homeDistanceToNearestDiamond > math.sqrt(72)):
+                self.goal_position = board.game_objects[diamondButton].position
+            else:
+                self.goal_position = board.game_objects[min].position
 
         if self.goal_position:
             # Calculate move according to goal position
