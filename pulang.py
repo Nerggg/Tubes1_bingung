2c9f93890620e813531db16060380d7c759ebd47
Christian Justin <christian.justin23@gmail.com>
2024-03-06 21:08:09 +0700

    Added Komentar
    Kayaknya udah cukup deh

--- pulang.py
+++ pulang.py
@@ -9,29 +9,41 @@ class Pulang(object):
         self.previous_position = (None, None) # previous_position adalah posisi sebelumnya yang diinisialisasi dengan (None, None) karena bot belum bergerak
         self.turn_direction = 1 # turn_direction adalah arah putaran bot yang diinisialisasi dengan 1 yang berarti ke kanan
     
+    # Fungsi ini digunakan untuk pulang ke base jika beberapa kondisi terpenuhi
     def go_home(self, board_bot: GameObject, board: Board):
         props = board_bot.properties
+
+        # Mendapatkan waktu yang tersisa dalam milisecond
         current_time = props.milliseconds_left
+
+        # Mengubah waktu yang tersisa dari milisecond ke detik
         new_time = current_time / 1000
+
         base = props.base
+
+        # Menghitung estimasi waktu yang dibutuhkan untuk sampai ke base dengan manhattan distance
         estimated_time_to_base = abs(base.x - board_bot.position.x) + abs(base.y - board_bot.position.y)
-        if current_time < 10000 and estimated_time_to_base > new_time or (props.diamonds in [3,4] and estimated_time_to_base < 4 ):
-            self.goal_position = base
+
+        # Jika waktu yang tersisa kurang dari 10 detik dan estimasi waktu yang dibutuhkan untuk sampai ke base lebih besar sama dengan dari waktu yang tersisa
+        # atau jika diamond yang dimiliki bot adalah 3 atau 4 dan estimasi waktu yang dibutuhkan untuk sampai ke base kurang dari 4
+        # Maka goal positionnya adalah base
+        if current_time < 10000 and estimated_time_to_base >= new_time or (props.diamonds in [3,4] and estimated_time_to_base < 4 ):
+            self.goal_position = base 
         else:
             self.goal_position = None
     
     # Fungsi ini digunakan untuk mencari diamond dengan rasio terbesar pada setiap langkahnnya
     # yang mana merupakan implementasi dari algoritma greedy
     def purge(self, board_bot: GameObject, board: Board):
-        # Mendapatkan semua diamond button, diamond, dan teleporter yang ada di board dengan cara mengiterasi semua game object pada board
-        # lalu mencari type yang diinginkan yakni "DiamondButtonGameObject" dan "DiamondGameObject"
+        # Mendapatkan semua diamond yang ada di board dengan cara mengiterasi semua game object pada board
+        # lalu mencari type yang diinginkan yakni "DiamondGameObject"
         diamonds = [go for go in board.game_objects if go.type == "DiamondGameObject"]
         
-        # Jika diamond button ada dan diamond tidak ada atau jumlah diamond kurang dari 3, maka goal position adalah diamond button pertama
+        # Menginisialisasi max_ratio dengan -1 dan min_index dengan None
         max_ratio = -1
         min_index = None
 
-        # Untuk setiap diamond, kalkulasi rasio dari diamond point kuadrat dengan jarak kuadrat antara bot dan diamond
+        # Untuk setiap diamond, kalkulasi rasio dari diamond point dengan jarak manhattan antara bot dan diamond
         for i, diamond in enumerate (diamonds):
             bot_diamonds = board_bot.properties.diamonds # Mendapatkan jumlah diamond yang dimiliki oleh bot
             diamond_points = diamond.properties.points # Mendapatkan point dari diamond
@@ -39,23 +51,20 @@ class Pulang(object):
             if (bot_diamonds + diamond_points > 5): # Jika jumlah diamond yang dimiliki bot setelah mengambil diamond ini lebih dari 5, maka skip diamond ini
                 continue
 
-            # Melakukan kalkulasi jarak kuadrat antara bot dan diamond
+            # Melakukan kalkulasi jarak manhattan antara bot dan diamond
             # Dengan cara mengurangi x dan y koordinat dari posisi diamond dengan posisi bot
-            # Lalu hasilnya dipangkatkan 2 untuk memastikan hasilnya positif 
+            # Lalu hasilnya diabsolutkan agar tidak ada nilai negatif
             x_dist = abs(board_bot.position.x - diamond.position.x)
             y_dist = abs(board_bot.position.y - diamond.position.y)
-            manhattan_dist = x_dist + y_dist
-            
-            # Jarak kuadrat di x dan y akhirnya dijumlahkan untuk memberikan nilai total jarak kuadrat dari bot ke diamond 
-            # Teknik penentuan total jarak ini menggunakan Teorema Phytagoras           
+            manhattan_dist = x_dist + y_dist        
 
-            # Jika jarak kuadratnya 0, berarti bot sudah berada di posisi diamond, yang berarti
+            # Jika jarak manhattannya 0, berarti bot sudah berada di posisi diamond, yang berarti
             # bot tidak lagi perlu melakukan kalkulasi ratio untuk diamond yang ini, jadi diamond yang ini akan diskip perhitungannya
             # dan akan langsung dilanjutkan ke diamond selanjutnya
             if manhattan_dist == 0: 
                 continue
             
-            # Mengkalkulasi rasio dari diamond point kuadrat dengan jarak kuadrat dari bot ke diamond
+            # Mengkalkulasi rasio dari diamond point dengan jarak dari bot ke diamond
             # Rasio ini akan digunakan untuk menentukan diamond mana yang akan dijadikan goal position
             # Diamond dengan point yang lebih tinggi dan jarak yang lebih dekat akan memiliki rasio lebih besar
             ratio = diamond_points / manhattan_dist
@@ -87,16 +96,16 @@ class Pulang(object):
         for teleporter in teleporters:
             # Ini akan melakukan pengecekan apakah bot berada dalam garis lurus antara teleporter dan goal position
             if self.is_in_line(board_bot.position, teleporter.position, self.goal_position):
-                # If the teleporter is to the right of the bot, move the goal to the left
+                # Jika teleporter berada di kiri bot, maka goal positionnya akan diubah ke kanan bot
                 if teleporter.position.x > board_bot.position.x:
                     self.goal_position.x = board_bot.position.x - 1
-                # If the teleporter is to the left of the bot, move the goal to the right
+                # Jika teleporter berada di kanan bot, maka goal positionnya akan diubah ke kiri bot
                 elif teleporter.position.x < board_bot.position.x:
                     self.goal_position.x = board_bot.position.x + 1
-                # If the teleporter is above the bot, move the goal down
+                # Jika teleporter berada di atas bot, maka goal positionnya akan diubah ke bawah bot
                 if teleporter.position.y > board_bot.position.y:
                     self.goal_position.y = board_bot.position.y - 1
-                # If the teleporter is below the bot, move the goal up
+                # Jika teleporter berada di bawah bot, maka goal positionnya akan diubah ke atas bot
                 elif teleporter.position.y < board_bot.position.y:
                     self.goal_position.y = board_bot.position.y + 1
     
@@ -110,22 +119,18 @@ class Pulang(object):
         dx2 = pos3.x - pos2.x
         dy2 = pos3.y - pos2.y
 
-        # Jika hasil perkalian antara dx1 dan dy2 sama dengan hasil perkalian antara dy1 dan dx2, yang berarti
-        # arab dari pos1 ke pos2 dan dari pos2 ke pos3 adalah sama
-        # Jadi, fungsinya akan menghasilkan true
+        # Jika posisi berada dalam satu garis lurus, maka dx1, dx2, dy1, dan dy2 akan bernilai 0
         if dx1 == dx2 == 0:
             return True
-
-        # If the positions are horizontally aligned, the differences in y will be 0
         if dy1 == dy2 == 0:
             return True
            
 
-        # If the slopes are equal, the positions are in a straight line
+        # Jika dx1 dan dx2 tidak 0, dan dy1/dx1 sama dengan dy2/dx2, maka posisi berada dalam satu garis lurus
         if dx1 != 0 and dx2 != 0 and dy1/dx1 == dy2/dx2:
             return True
 
-        # If none of the above conditions are met, the positions are not in a straight line
+        # Jika tidak memenuhi kondisi, maka posisi tidak berada dalam satu garis lurus
         return False
 
     # Fungsi ini digunakan untuk melakukan kalkulasi untuk gerakan selanjutnya dari bot
@@ -140,10 +145,12 @@ class Pulang(object):
             self.goal_position = base
         
         else:
+            # Memanggil metode go_home untuk mengecek apakah bot harus pulang ke base
+            # Jika iya, maka goal positionnya adalah base
             self.go_home(board_bot, board)
             if self.goal_position is None:
-                # Jika belum terkumpul 5 diamond, maka akan memanggil fungsi purge untuk mencari diamond dengan rasio terbesar
-                # Yang mana sudah dijelaskan fungsi purge untuk apa di atas
+                # Memanggil metode purge untuk mencari diamond dengan rasio terbesar
+                # yang akan dijadikan goal position
                 self.purge(board_bot,board)
     
         # Jika bot sudah mempunyai posisi goal, maka akan dihitung gerakan untuk mendekati posisi tersebut
